79a11cd6795d6b5c524de3d7de8f433675e8bb73
tanshengmei <TanShengMei@gds-services.com>
2020-07-29 22:33:56 +0800

    搬砖ing

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ $(function() {
     el: '#main',
     data: {
       leftBar: false,
+      rightBar: false,
+      hideSuspend: false,
+      settingVisible: false,
       nav: [
         { id: 1, name: '菜谱', icon: 'el-icon-food' },
         { id: 2, name: '厨师', icon: 'el-icon-user' },
@@ -15,11 +18,35 @@ $(function() {
         { id: 9, name: '说明', icon: 'el-icon-info' },
       ],
       navId: 1,
+      tableHeight: window.innerHeight - 122,
       data: [],
       recipes: [],
-      recipes_page: [],
+      recipesPage: [],
+      repCol: {
+        rarity: false,
+        skills: false,
+        price: true,
+        time: true,
+        gold_eff: true,
+        origin: true,
+      },
+      repKeyword: '',
       recipesCurPage: 1,
       recipesPageSize: 20,
+      questsType: 1,
+      questsTypes: [{
+        value: 1,
+        label: '主线任务'
+      }, {
+        value: 2,
+        label: '支线任务'
+      }],
+      questsKeyword: '',
+      questsMain: [],
+      questsPage: [],
+      questsRegional: [],
+      questsCurPage: 1,
+      questsPageSize: 20,
     },
     mounted() {
       this.loadData();
@@ -31,32 +58,150 @@ $(function() {
         }).then(rst => {
           console.log(rst);
           this.data = rst;
-          this.initData();
+          this.initRep();
         });
       },
       checkNav(id) {
         this.navId = id;
         this.leftBar = false;
       },
-      initData() {
-        this.recipes = this.data.recipes.map(item => {
+      initRep() {
+        this.recipes = [];
+        for (const item of this.data.recipes) {
           item.rarity_show = '★★★★★'.slice(0, item.rarity);
           const skill_arr = ['stirfry', 'boil', 'knife', 'fry', 'bake', 'steam'];
           for (let i of skill_arr) {
             item[i] = item[i] || null;
           }
-          return item;
-        });
-        this.recipes_page = this.recipes.slice(0, this.recipesPageSize);
+          item.materials_show = item.materials.map(m => {
+            const name = this.data.materials.find(i => {
+              return i.materialId == m.material;
+            }).name;
+            return `${name}*${m.quantity}`;
+          }).join(' ');
+          const materials_search = item.materials.map(m => {
+            const name = this.data.materials.find(i => {
+              return i.materialId == m.material;
+            }).name;
+            return name;
+          }).join(' ');
+          item.origin = item.origin.replace('<br>', '\n');
+          item.time_show = (item.time >= 60 ? `${~~(item.time / 60)}分` : '') + (item.time % 60 !== 0 ? `${item.time % 60}秒` : '');
+          item.gold_eff = Math.round(3600 / item.time * item.price);
+          const s_name = item.name.indexOf(this.repKeyword) > -1;
+          const s_origin = item.origin.indexOf(this.repKeyword) > -1;
+          const s_material = materials_search.indexOf(this.repKeyword) > -1;
+          if (s_name || s_origin || s_material) {
+            this.recipes.push(item);
+          }
+        }
+        this.recipesPage = this.recipes.slice(0, this.recipesPageSize);
       },
       handleCurrentChange(val) {
         if (this.navId === 1) {
           this.recipesCurPage = val;
           const size = this.recipesPageSize;
-          this.recipes_page = this.recipes.slice((val - 1) * size, val * size);
+          this.recipesPage = this.recipes.slice((val - 1) * size, val * size);
+        } else if (this.navId === 6) {
+          this.questsCurPage = val;
+          const size = this.questsPageSize;
+          const quests = this.questsType === 1 ? this.questsMain : this.questsRegional;
+          this.questsPage = quests.slice((val - 1) * size, val * size);
+          this.$refs.questsTable.bodyWrapper.scrollTop = 0;
         }
+      },
+      handleRepSort(sort) {
+        const map = {
+          time_show: 'time',
+          rarity_show: 'rarity'
+        };
+        if (!sort.order) {
+          this.initRep();
+        }
+        sort.prop = map[sort.prop] || sort.prop;
+        this.recipesCurPage = 1;
+        this.recipes.sort(this.customSort(sort));
+        this.recipesPage = this.recipes.slice(0, this.recipesPageSize);
+      },
+      handleQuestsSort(sort) {
+        if (!sort.order) {
+          this.initQuests();
+        }
+        this.questsCurPage = 1;
+        if (this.questsType === 1) {
+          this.questsMain.sort(this.customSort(sort));
+          this.questsPage = this.questsMain.slice(0, this.questsPageSize);
+        } else {
+          this.questsRegional.sort(this.customSort(sort));
+          this.questsPage = this.questsRegional.slice(0, this.questsPageSize);
+        }
+      },
+      customSort(sort) {
+        const map = {
+          ascending: -1,
+          descending: 1,
+        }
+        function sortFunc(x, y) {
+          if (x[sort.prop] < y[sort.prop]) {
+            return map[sort.order];
+          } else if (x[sort.prop] > y[sort.prop]) {
+            return 0 - map[sort.order];
+          } else {
+            return 0;
+          }
+        }
+        return sortFunc;
+      },
+      initQuests() {
+        const key = this.questsKeyword;
+        this.questsCurPage = 1;
+        this.questsMain = [];
+        this.questsRegional = [];
+        for (let item of this.data.quests) {
+          const rewards = item.rewards.map(r => {
+            return r.quantity ? `${r.name} * ${r.quantity}` : r.name;
+          });
+          item.rewards_show = rewards.join('\n');
+          const search = String(item.questId).indexOf(key) > -1 || item.goal.indexOf(key) > -1 || item.rewards_show.indexOf(key) > -1;
+          if (item.type === '主线任务' && search) {
+            this.questsMain.push(item);
+          } else if (item.type === '支线任务' && search) {
+            this.questsRegional.push(item);
+          }
+        }
+        this.questsPage = this.questsType == 1 ? this.questsMain.slice(0, this.questsPageSize) : this.questsRegional.slice(0, this.questsPageSize);
+        this.$nextTick(() => {
+          this.$refs.questsTable.bodyWrapper.scrollTop = 0;
+          this.$refs.questsTable.clearSort();
+        });
+      },
+      reset() {
+        //
       }
     },
-    watch: {}
+    watch: {
+      repKeyword() {
+        this.initRep();
+      },
+      questsType() {
+        this.initQuests();
+      },
+      questsKeyword() {
+        this.initQuests();
+      },
+      navId(val) {
+        console.log(val);
+        if (val === 1) {
+          this.initRep();
+          this.$nextTick(()=>{
+            this.$refs.recipesTable.bodyWrapper.scrollTop = 0;
+            this.$refs.recipesTable.bodyWrapper.scrollLeft = 0;
+            this.$refs.recipesTable.doLayout();
+          });
+        } else if (val === 6) {
+          this.initQuests();
+        }
+      }
+    }
   });
 });
