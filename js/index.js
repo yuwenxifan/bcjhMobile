6af72c9e0467f21f695935ea479fa4ab60688d81
yuwenxifan <tanshengmeistc@126.com>
2020-09-05 12:43:54 +0800

    我一定是个傻的

--- js/index.js
+++ js/index.js
@@ -890,7 +890,7 @@ $(function() {
       },
       loadFoodGodRule() {
         $.ajax({
-          url: './data/foodgodRule.min.json?v=8'
+          url: './data/foodgodRule.min.json?v=9'
         }).then(rst => {
           const now = new Date();
           if (new Date(rst.startTime) <= now && new Date(rst.endTime) >= now) {
