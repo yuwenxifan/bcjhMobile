a8b3109df6a6053ba896bfd5f55a4b1bbf7e4b0d
yuwenxifan <tanshengmeistc@126.com>
2020-09-05 13:41:19 +0800

    绿叶宴是五倍呀

--- js/index.js
+++ js/index.js
@@ -890,7 +890,7 @@ $(function() {
       },
       loadFoodGodRule() {
         $.ajax({
-          url: './data/foodgodRule.min.json?v=9'
+          url: './data/foodgodRule.min.json?v=10'
         }).then(rst => {
           const now = new Date();
           if (new Date(rst.startTime) <= now && new Date(rst.endTime) >= now) {
