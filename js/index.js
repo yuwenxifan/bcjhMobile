f1bbea636f2dcd49f61a0527d0957b4b4db69cf5
tanshengmei <TanShengMei@gds-services.com>
2020-09-18 14:15:48 +0800

    _(:з)∠)_

--- js/index.js
+++ js/index.js
@@ -890,7 +890,7 @@ $(function() {
       },
       loadFoodGodRule() {
         $.ajax({
-          url: './data/foodgodRule.min.json?v=14'
+          url: './data/foodgodRule.min.json?v=15'
         }).then(rst => {
           const now = new Date();
           if (new Date(rst.startTime) <= now && new Date(rst.endTime) >= now) {
