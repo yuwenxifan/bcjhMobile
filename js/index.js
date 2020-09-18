84c7bd16db4331db8313459b6efa17c3fad6a99e
tanshengmei <TanShengMei@gds-services.com>
2020-09-18 14:04:17 +0800

    御前有啦

--- js/index.js
+++ js/index.js
@@ -890,7 +890,7 @@ $(function() {
       },
       loadFoodGodRule() {
         $.ajax({
-          url: './data/foodgodRule.min.json?v=13'
+          url: './data/foodgodRule.min.json?v=14'
         }).then(rst => {
           const now = new Date();
           if (new Date(rst.startTime) <= now && new Date(rst.endTime) >= now) {
