1e8e54bacd773ff74ae5e0f1d0fb7f4dc6ff9a44
tanshengmei <TanShengMei@gds-services.com>
2020-09-18 16:58:08 +0800

    非御前规则有啦

--- js/index.js
+++ js/index.js
@@ -890,7 +890,7 @@ $(function() {
       },
       loadFoodGodRule() {
         $.ajax({
-          url: './data/foodgodRule.min.json?v=15'
+          url: './data/foodgodRule.min.json?v=16'
         }).then(rst => {
           const now = new Date();
           if (new Date(rst.startTime) <= now && new Date(rst.endTime) >= now) {
