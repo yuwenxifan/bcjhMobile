41aca2ca99ef97cde3c90f274e8d75c19a04eb11
yuwenxifan <tanshengmeistc@126.com>
2022-08-16 21:17:05 +0800

    啊，忙忘了

--- js/index.js
+++ js/index.js
@@ -1340,7 +1340,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=47'
+          url: './data/data.min.json?v=48'
         }).then(rst => {
           this.data = rst;
           this.initData();
