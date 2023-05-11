8876aaf13f0b8df2481e3029c7aa08143fc96fdd
TanShengmei <tanshengmei@gds-services.com>
2023-05-11 18:25:02 +0800

    麻了呀

--- js/index.js
+++ js/index.js
@@ -1370,7 +1370,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=67'
+          url: './data/data.min.json?v=68'
         }).then(rst => {
           this.data = rst;
           this.initData();
