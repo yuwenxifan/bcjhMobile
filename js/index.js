7107ad32afa55be0edbeb65b3844f52ab1f64f13
TanShengmei <tanshengmei@gds-services.com>
2024-03-22 13:03:11 +0800

    :sparkles:

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=81'
+          url: './data/data.min.json?v=82'
         }).then(rst => {
           this.data = rst;
           this.initData();
