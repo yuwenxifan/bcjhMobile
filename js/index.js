e69240a81eb1b71096602cdf001cfee3182861e9
喻文稀饭 <tanshengmeistc@126.com>
2024-07-29 21:02:13 +0800

    :sparkles: 才发现又出了礼包厨，差点忘了买都

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=88'
+          url: './data/data.min.json?v=89'
         }).then(rst => {
           this.data = rst;
           this.initData();
