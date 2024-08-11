a884ff7e36561534d95e23d34308ec8f21bf0dce
喻文稀饭 <tanshengmeistc@126.com>
2024-08-11 20:41:40 +0800

    :wrench: 怎么性别错了

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=90'
+          url: './data/data.min.json?v=91'
         }).then(rst => {
           this.data = rst;
           this.initData();
