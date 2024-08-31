ba47701d5cb3265e82df3e1dbd23a10f4cb89ecc
喻文稀饭 <tanshengmeistc@126.com>
2024-08-31 15:44:30 +0800

    :sparkles: 感觉礼包厨越出越频繁了

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=91'
+          url: './data/data.min.json?v=92'
         }).then(rst => {
           this.data = rst;
           this.initData();
