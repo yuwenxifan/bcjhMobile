4993271f73fb41fc6cb61d7e933a5a08e63c9087
喻文稀饭 <tanshengmeistc@126.com>
2024-11-10 23:22:07 +0800

    :sparkles: 更新

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=95'
+          url: './data/data.min.json?v=96'
         }).then(rst => {
           this.data = rst;
           this.initData();
