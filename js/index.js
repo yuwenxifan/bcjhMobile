08f00d8f557b2b8df545848a4eec9c50c992241e
喻文稀饭 <tanshengmeistc@126.com>
2025-02-02 19:43:23 +0800

    一觉醒来天塌了

--- js/index.js
+++ js/index.js
@@ -1585,7 +1585,7 @@ $(function() {
       },
       loadData() {
         $.ajax({
-          url: './data/data.min.json?v=99'
+          url: './data/data.min.json?v=100'
         }).then(rst => {
           this.data = rst;
           this.initData();
